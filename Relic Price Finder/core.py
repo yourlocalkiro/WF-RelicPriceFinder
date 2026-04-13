cb53b4f48c543a11219e7f8dfae16da1aebd68a0
KI709 <ki.chromium@gmail.com>
2026-04-13 23:21:47 +0530

    Fourth Commit

--- Relic Price Finder/core.py
+++ Relic Price Finder/core.py
@@ -2,6 +2,15 @@ import aiohttp
 import asyncio
 import requests
 
+import time
+
+CACHE = {
+    "data": None,
+    "timestamp": 0
+}
+
+CACHE_DURATION = 300  # 5 minutes
+
 # ---------------------------
 # CONFIG
 # ---------------------------
@@ -193,6 +202,14 @@ def calculate_best_relic(relic_map, item_prices):
     return best[:5]
 
 def get_top_items():
+    global CACHE
+
+    now = time.time()
+
+    # ✅ RETURN CACHED DATA
+    if CACHE["data"] and now - CACHE["timestamp"] < CACHE_DURATION:
+        print("Using cached data...")
+        return CACHE["data"]
     data = get_data()
 
     active_relics = get_unvaulted_relics(data)
@@ -225,10 +242,16 @@ def get_top_items():
 
     best_relics = calculate_best_relic(relic_map, item_prices)
 
-    return {
+    result = {
         "items": output,
         "best_relics": best_relics
     }
+
+    # ✅ SAVE TO CACHE
+    CACHE["data"] = result
+    CACHE["timestamp"] = now
+
+    return result
 
 
 # ---------------------------
